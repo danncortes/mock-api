c270925bfa414088ebd3b4db7d6c814dc88aecb6
danncortes <danncortes@gmail.com>
2018-02-07 16:43:42 -0500

    Moving to Github and adding eslint

--- mock-data-schema.js
+++ mock-data-schema.js
@@ -1,36 +1,36 @@
-var schema = {
-    "type": "object",
-    "properties": {
-        "users": {
-            "type": "array",
-            "minItems": 4,
-            "maxItems": 10,
-            "items": {
-                "type": "object",
-                "properties": {
-                    "id": {
-                        "type": "number",
-                        "unique": true,
-                        "minimum": 1
+const schema = {
+    type: 'object',
+    properties: {
+        users: {
+            type: 'array',
+            minItems: 4,
+            maxItems: 10,
+            items: {
+                type: 'object',
+                properties: {
+                    id: {
+                        type: 'number',
+                        unique: true,
+                        minimum: 1,
                     },
-                    "firstName": {
-                        "type": "string",
-                        "faker": "name.firstName"
+                    firstName: {
+                        type: 'string',
+                        faker: 'name.firstName',
                     },
-                    "lastName": {
-                        "type": "string",
-                        "faker": "name.lastName"
+                    lastName: {
+                        type: 'string',
+                        faker: 'name.lastName',
                     },
-                    "email": {
-                        "type": "string",
-                        "faker": "internet.email"
-                    }
+                    email: {
+                        type: 'string',
+                        faker: 'internet.email',
+                    },
                 },
-                "required": ["id", "type", "lastname", "email"]
-            }
-        }
+                required: ['id', 'type', 'lastname', 'email'],
+            },
+        },
     },
-    "required": ["users"]
+    required: ['users'],
 };
 
 module.exports = schema;
